1d6d29d21e6d0c33870c1fbd362faebdf548a366
Shubhan-siri <shubhanshubusines786@gmail.com>
2025-08-17 22:23:20 +0530

    Update python3 small_code.py

--- python3 small_code.py
+++ python3 small_code.py
@@ -1,10 +1,10 @@
 # 1
 print("Hello Shubhanshu Sen")
 # 2
-num = 10
+num = 5
 # 3
 print("Enter a number:", num)
 # 4
 print("Square of the number is", num * num)
 # 5
-print("Have a great day!")
+print("Have a great day Boy!")
